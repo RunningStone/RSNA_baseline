777b3c514f14817318fd08dc49ffcf2bd4d35740
shipan <shipan@shipanmbp.home>
2023-01-18 22:54:42 +0100

    change metrics to fit new pl version

--- pipeline/metrics.py
+++ pipeline/metrics.py
@@ -2,28 +2,30 @@ import torchmetrics
 def create_metrics(n_classes,):
             #---->Metrics
         if n_classes > 2: 
-            metrics_template = torchmetrics.MetricCollection([torchmetrics.Accuracy(num_classes = n_classes,
+            metrics_template = torchmetrics.MetricCollection([torchmetrics.Accuracy(task="multiclass",
+                                                                            num_classes = n_classes,
                                                                            average='micro'),
                                                     
-                                                     torchmetrics.CohenKappa(num_classes = n_classes),
-                                                     torchmetrics.F1Score(num_classes = n_classes,
+                                                     torchmetrics.CohenKappa(task="multiclass",num_classes = n_classes),
+                                                     torchmetrics.F1Score(task="multiclass",num_classes = n_classes,
                                                                      average = 'macro'),
-                                                     torchmetrics.Recall(average = 'macro',
+                                                     torchmetrics.Recall(task="multiclass",average = 'macro',
                                                                          num_classes = n_classes),
-                                                     torchmetrics.Precision(average = 'macro',
+                                                     torchmetrics.Precision(task="multiclass",average = 'macro',
                                                                             num_classes = n_classes),
-                                                     torchmetrics.Specificity(average = 'macro',
+                                                     torchmetrics.Specificity(task="multiclass",average = 'macro',
                                                                             num_classes = n_classes)])
         else : 
-            metrics_template = torchmetrics.MetricCollection([torchmetrics.Accuracy(num_classes = 2,
+            metrics_template = torchmetrics.MetricCollection([torchmetrics.Accuracy(task="binary",
+                                                                            num_classes = 2,
                                                                            average = 'micro'),
-                                                    torchmetrics.AUROC(num_classes = n_classes, average = 'macro'),
-                                                     torchmetrics.CohenKappa(num_classes = 2),
-                                                     torchmetrics.F1Score(num_classes = 2,
+                                                    torchmetrics.AUROC(task="binary",num_classes = n_classes, average = 'macro'),
+                                                     torchmetrics.CohenKappa(task="binary",num_classes = 2),
+                                                     torchmetrics.F1Score(task="binary",num_classes = 2,
                                                                      average = 'macro'),
-                                                     torchmetrics.Recall(average = 'macro',
+                                                     torchmetrics.Recall(task="binary",average = 'macro',
                                                                          num_classes = 2),
-                                                     torchmetrics.Precision(average = 'macro',
+                                                     torchmetrics.Precision(task="binary",average = 'macro',
                                                                             num_classes = 2)])
         return metrics_template
 
